e6ce870e63ab8b2d69f9e91aa70fbb6e8ea4514c
Dongsoon-Shin <tkskttk@gmail.com>
2020-03-24 08:06:49 +0900

    dist update:

--- macOS/mac_test.py
+++ macOS/mac_test.py
@@ -1,4 +1,4 @@
-# -*- UTF-8 encoding -*-
+# -*- coding:utf-8 -*-
 import time
 import selenium_method
 import fuzzer_method
@@ -7,38 +7,41 @@ import fuzzer_method
 
 def DeviceTesting(start):
     dv = fuzzer_method.fuzz(max_length=5)
-    print("first dv", dv)
     start.AddDevice(driver)
     start.TypeString(driver, str(dv))
     start.Confirm(driver)
     start.AddDeviceDetail(driver, str(dv), str(dv))
-    for num in range(3):
+    for num in range(20):
         start.AddDeviceTag(driver, str(num))
 
-    print("second dv", dv)
-    ret2 = start.AddDeviceOfDevice(driver, str(dv))
-    start.Confirm(driver)
-    start.AddDeviceDetail(driver, str(dv), str(ret2))
-    for i in range(3):
-        start.AddDeviceTag(driver, str(i))
+    for j in range(200):
+        ret2 = start.AddDeviceOfDevice(driver, str(dv))
+        start.Confirm(driver)
+        start.AddDeviceDetail(driver, str(dv), str(ret2))
+        time.sleep(0.1)
+        # for i in range(9):
+        #     start.AddDeviceTag(driver, str(i))
     
     return dv
 
 def ServerTesting(start, dv):
-    for i in range(3):
+    for i in range(200):
         start.AddServer(driver)
         start.AddServerDetail(driver)
-        for j in range(1,3):
+        for j in range(1,21):
             start.AddServerTag(driver, j, dv)
 
 if __name__ == "__main__":
 
+    # Init Chrome Driver and Selenium
     chrome = selenium_method.InitDriver()
     driver = chrome.SetChrome()
     driver.get('localhost:1290')
 
+    # Create object that selenium running module
     start = selenium_method.AddToContents()
     start.ConfigureClear(driver)
+
 
     # running time measure
     start_time = time.time()
