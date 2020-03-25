4e2c24cf18f845798aa14ecf3624f7461188a442
Dongsoon-Shin <tkskttk@gmail.com>
2020-03-25 13:36:36 +0900

    delete the fuzzer file

--- Ubuntu/fuzzer_method.py
+++
@@ -1,9 +0,0 @@
-import os, inspect, subprocess, random
-
-def fuzz(max_length = 100, start = 32, end = 32):
-    out = ""
-    string_length = random.randrange(max_length) + 1
-    for i in range(string_length):
-        out += chr(random.randint(start, start+end))
-
-    return out
--- macOS/fuzzer_method.py
+++
@@ -1,9 +0,0 @@
-import os, inspect, subprocess, random
-
-def fuzz(max_length = 100, start = 64, end = 126):
-    out = ""
-    string_length = random.randrange(max_length) + 1
-    for i in range(string_length):
-        out += chr(random.randint(start, end))
-
-    return out
