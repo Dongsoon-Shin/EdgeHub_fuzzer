188c2c5cdf04a8582979421056002d8919388f5f
Dongsoon Shin <tkskttk@gmail.com>
2020-04-20 09:07:24 +0900

    Update Windows_test.py

--- Windows/Windows_test.py
+++ Windows/Windows_test.py
@@ -2,26 +2,25 @@
 from Edgehub_testing_module import fuzzer_method, selenium_method, crolling, Device_module
 
 if __name__ == "__main__":
-    # Device interval, Time out, count
-    # Device_module.ModbusTCP_test(1000, 100, 1)
-    # Device_module.ModbusTCP_test(1000, 100, 2)
-    # Device_module.ModbusTCP_test(1000, 100, 4)
-    # Device_module.ModbusTCP_test(1000, 100, 8)
-    # Device_module.ModbusTCP_test(1000, 100, 16)
-    # Device_module.ModbusTCP_test(1000, 100, 32)
-    # Device_module.ModbusTCP_test(1000, 100, 64)
-    # Device_module.ModbusTCP_test(1000, 100, 128)
-    # Device_module.ModbusTCP_test(1000, 100, 256)
-    # Device_module.ModbusTCP_test(1000, 100, 512)
-    Device_module.ModbusTCP_test(1000, 100, 1024)
-    # Device_module.ModbusTCP_test(1000, 100, 2048)
-    # Device_module.ModbusTCP_test(1000, 100, 4096)
-    # Device_module.ModbusTCP_test(1000, 100, 8192)
+    chrome = selenium_method.InitDriver()
+    driver = chrome.SetChrome()
+    
+    driver.get("http://localhost:1290/device?category=device&name=d&line=g&index=0")
+    
+    for i in range(1,1000):
+        selenium_method.ByIdClicking(driver, f'datamapperAdd')
+        selenium_method.ByIdSendKey(driver, f'datamapperId_0', str(i))
+        selenium_method.ByIdSendKey(driver, f'datamapperInfo_id', str(1))
+        selenium_method.ByIdSendKey(driver, f'datamapperInfo_address', str(i))
+        selenium_method.ByIdSendKey(driver, f'datamapperInfo_length', str(1))
 
+        selenium_method.ByIdClicking(driver, f'datamapperInfo_valueType__BV_toggle_')
+        selenium_method.ByIdClicking(driver, f'datamapperInfo_valueType_int')
 
-    # Device_module.ModbusRTU_test()
-    # Device_module.Fenet_test()
-    # Device_module.Cnet_test()
-    # Device_module.MELSEC_SERIAL_test()
-    # Device_module.MELSEC_ETH_test()
+        selenium_method.ByIdSendKey(driver, f'datamapperInterval', str(1000))
+
+        selenium_method.ByIdClicking(driver, f'datamapperAddDone')
+
+    
+
     
