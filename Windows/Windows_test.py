85d30ff094928339923caf12968cb19167f09a4c
Dongsoon Shin <tkskttk@gmail.com>
2020-04-24 13:06:25 +0900

    Update

--- Windows/Windows_test.py
+++ Windows/Windows_test.py
@@ -2,4 +2,4 @@
 from Edgehub_testing_module import fuzzer_method, selenium_method, crolling, Device_module
 
 if __name__ == "__main__":
-    Device_module.ModbusTCP_test(1000, 10000, 4)
+    Device_module.ModbusTCP_test(1000, 10000, 64)
